debc35de41e0c1eaa6ba3fcf0655daac4a707c62
popovay <90705081+popovay@users.noreply.github.com>
2021-10-16 17:34:18 +0400

    это файл из курса первого года

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "TXLib.h"
+using namespace std;
+
+
+int main()
+{
+    int x=50,y=50;
+    int w=50, h=200;
+    txCreateWindow(600,600);
+    int n=1;
+
+    for (int i=1;i<7;i=i+1)
+    {
+    if(n<4){txFillColour(TX_YELLOW); n=n+1}
+    else {txFillColour(TX_GREEN);n=1;}
+    txRectangle(x,y,x+w,y+h);
+    x=x+70;
+
+    }
+    return 0;
+}
